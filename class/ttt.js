5551315b3c26dece037db8ea21188bf317a347b9
Sujal Shrestha <84851820+SuzalShrestha@users.noreply.github.com>
2023-02-27 13:43:27 +0000

    Completed

--- class/ttt.js
+++ class/ttt.js
@@ -29,11 +29,35 @@ class TTT {
   }
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    let data=
+    {
+      x:"X",o:"O",space:" ",xcount:0,ocount:0,spaceCount:0
+    }
+    let updateDate=()=>{
+    for(let i=0;i<3;i++){
+      let first=grid[i];
+      first.forEach(element => {
+          if(element===data.x){
+            data.xcount+=1;
+          }else if(element===data.o){
+            data.ocount+=1;
+          }else{
+            data.spaceCount+=1;
+          }
+      });
+    }
+    }
+    updateDate();
+      if(data.spaceCount===9){
+        return false;
+      }
+    if(data.ocount>=3 || data.xcount>=3){
+      
+    }
 
   }
 
